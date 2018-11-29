9032e2700bdaaea336c43ff2f8c88fe94c42cbb9
Rolf van Kleef <rolf@vankleef.me>
2018-11-29 16:33:05 +0100

    Added more useful admin UI shite.

--- steambird/admin.py
+++ steambird/admin.py
@@ -1,28 +1,45 @@
 from django.contrib import admin
-from polymorphic.admin import PolymorphicParentModelAdmin, PolymorphicChildModelAdmin, PolymorphicChildModelFilter
+from polymorphic.admin import PolymorphicParentModelAdmin, \
+    PolymorphicChildModelAdmin, PolymorphicChildModelFilter
 
 from .models import StudyMaterialEdition, Book, ScientificArticle, \
-    OtherMaterial, Course, Study, StudyCourse, Teacher, Module, StudyMaterial
+    OtherMaterial, Course, Study, StudyCourse, Teacher, Module, StudyMaterial, \
+    MaterialSelectionProcess
 
 admin.site.register(StudyCourse)
-admin.site.register(Teacher)
+
+
+@admin.register(MaterialSelectionProcess)
+class MaterialSelectionProcessAdmin(admin.ModelAdmin):
+    search_fields = ('osiris_specified_material__name',
+                     'approved_material__name')
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    search_fields = ('titles', 'initials', 'first_name', 'surname_prefix',
+                     'last_name', 'email')
+    list_display = ('__str__', 'email', 'last_login')
 
 
 @admin.register(Module)
 class ModuleAdmin(admin.ModelAdmin):
     list_display = ('name', 'course_code', 'module_moment')
     list_filter = ('module_moment', 'course__name')
+    autocomplete_fields = ('coordinator',)
 
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     list_display = ('name', 'year', 'course_code')
     list_filter = ('study__name',)
+    autocomplete_fields = ('teachers', 'materials')
 
 
 @admin.register(StudyMaterial)
 class StudyMaterialAdmin(admin.ModelAdmin):
-    list_display = ('id',)
+    list_display = ('name',)
+    search_fields = ('name',)
 
 
 @admin.register(Study)
@@ -34,39 +51,42 @@ class StudyAdmin(admin.ModelAdmin):
 class StudyMaterialChildAdmin(PolymorphicChildModelAdmin):
     base_model = StudyMaterialEdition
     base_fieldsets = (None, {
-        'fields': ('name',)
+        '': ('name', 'material_type')
     })
+    fields = ('name', 'material_type')
+    autocomplete_fields = ('material_type',)
 
 
 @admin.register(Book)
 class BookAdmin(StudyMaterialChildAdmin):
     base_model = Book
     show_in_index = True
-    fields = ('name', 'ISBN', 'author', 'img', 'year_of_publishing')
+    fields = StudyMaterialChildAdmin.fields + \
+             ('ISBN', 'author', 'img', 'year_of_publishing')
     list_display = ('name', 'ISBN', 'author', 'year_of_publishing')
-    list_filter = ('material_type__id', 'year_of_publishing')
+    list_filter = ('material_type', 'year_of_publishing')
 
 
 @admin.register(ScientificArticle)
 class ScientificArticleAdmin(StudyMaterialChildAdmin):
     base_model = ScientificArticle
     show_in_index = True
-    fields = ('name', 'DOI', 'author', 'year_of_publishing')
+    fields = ('name', 'material_type', 'DOI', 'author', 'year_of_publishing')
     list_display = ('name', 'DOI', 'author', 'year_of_publishing')
-    list_filter = ('material_type__id', 'year_of_publishing')
+    list_filter = ('material_type', 'year_of_publishing')
 
 
 @admin.register(OtherMaterial)
 class OtherMaterialAdmin(StudyMaterialChildAdmin):
     base_model = OtherMaterial
     show_in_index = True
-    fields = ('name',)
+    fields = ('name', 'material_type')
     list_display = ('name',)
-    list_filter = ('material_type__id',)
+    list_filter = ('material_type',)
 
 
 @admin.register(StudyMaterialEdition)
 class StudyMaterialParentAdmin(PolymorphicParentModelAdmin):
     base_model = StudyMaterialEdition
     child_models = (Book, ScientificArticle, OtherMaterial)
-    list_filter = (PolymorphicChildModelFilter,)
+    list_filter = (PolymorphicChildModelFilter, 'material_type')
