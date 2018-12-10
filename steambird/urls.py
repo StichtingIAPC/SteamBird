9d59b9034567bb6a79bae984ae9586a5db033af4
Rolf van Kleef <rolf@vankleef.me>
2018-12-10 20:14:10 +0100

    Made the admin interface massively more ugly.

--- steambird/urls.py
+++ steambird/urls.py
@@ -1,20 +1,6 @@
-"""steambird URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 from pysidian_core.urls import urls as pysidian_core_urls
 
@@ -23,16 +9,15 @@ from steambird.views import HomeView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='index'),
-    path('admin', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('admin', RedirectView.as_view(pattern_name='admin:index',
+                                       permanent=False)),
 ] + pysidian_core_urls
 
 if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns = [
-                      path('__debug__/', include(debug_toolbar.urls)),
-                      path('translations/', include('rosetta.urls'))
-
-                      # For django versions before 2.0:
-
-                  ] + urlpatterns
+        path('__debug__/', include(debug_toolbar.urls)),
+        path('translations/', include('rosetta.urls'))
+    ] + urlpatterns
