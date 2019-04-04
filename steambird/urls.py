20b14324d0813cbc98ff60528fc4f1ad4966ea4d
Luuk Lenders <luuklsll@gmail.com>
2019-04-04 20:19:35 +0200

    Small edits to make every 'app' have a working landing page

--- steambird/urls.py
+++ steambird/urls.py
@@ -5,12 +5,14 @@ from django.views.generic import RedirectView
 from pysidian_core.urls import urls as pysidian_core_urls
 
 from steambird import settings
+from steambird.views import IndexView
 
 urlpatterns = [
                   path('select2/', include('django_select2.urls')),
                   path('admin/', admin.site.urls),
                   path('admin', RedirectView.as_view(pattern_name='admin:index',
                                                      permanent=False)),
+                  path('', IndexView.as_view(), name='index'),
                   path('teacher/', include('steambird.teacher.urls', namespace='teacher')),
                   path('boecie/', include('steambird.boecie.urls')),
 
