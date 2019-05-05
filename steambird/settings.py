64f00a29ce76912969feccdd9b74606cf3a4c2a1
Rolf H. B. van Kleef <rkleef@iapc.utwente.nl>
2019-05-05 14:13:25 +0200

    Godverdomme wat is Django af en toe een stuk poepsoftware.

--- steambird/settings.py
+++ steambird/settings.py
@@ -155,3 +155,4 @@ LOCALE_PATHS = (os.path.join(BASE_DIR, 'locale'),)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
