b1aac3de62676fc3cee3e60787bcf035ceade4a4
Rolf H. B. van Kleef <rkleef@iapc.utwente.nl>
2019-05-29 13:01:14 +0200

    Fix indenting in new_book.js

--- steambird/static/js/steambird/forms/new_book.js
+++ steambird/static/js/steambird/forms/new_book.js
@@ -10,26 +10,26 @@
  * @returns {any}
  */
 $.fn.enterKey = function (fnc) {
-	return this.each(function () {
-		$(this).keypress(function (ev) {
-			let keycode = (ev.keyCode ? ev.keyCode : ev.which);
-			if (keycode == '13') {
-				fnc.call(this, ev);
-			}
-		})
-	})
+    return this.each(function () {
+        $(this).keypress(function (ev) {
+            let keycode = (ev.keyCode ? ev.keyCode : ev.which);
+            if (keycode == '13') {
+                fnc.call(this, ev);
+            }
+        });
+    });
 };
 
 /**
  * @param isbn {string}
  */
 function searchIsbn(isbn) {
-	$.ajax({
-		url: "/teacher/api/isbn/search",
-		data: {
-			isbn,
-		},
-	}).done(updateBook);
+    $.ajax({
+        url: "/teacher/api/isbn/search",
+        data: {
+           isbn,
+        },
+    }).done(updateBook);
 }
 
 /** @param response {{
@@ -52,36 +52,36 @@ function updateBook(response) {
     let book = response.meta;
 
     $("#material-type").val($('#type-add').val());
-	$("#book-add-isbn").val(book["ISBN-13"]);
-	$("#book-add-title").val(book.Title);
-	$("#book-add-author").val(book.Authors.join(", "));
-	$("#book-add-year").val(book.Year);
-	$("#book-add-image").val(book.thumbnail);
-	$("#book-add-show-image").attr("src", book.thumbnail);
+    $("#book-add-isbn").val(book["ISBN-13"]);
+    $("#book-add-title").val(book.Title);
+    $("#book-add-author").val(book.Authors.join(", "));
+    $("#book-add-year").val(book.Year);
+    $("#book-add-image").val(book.thumbnail);
+    $("#book-add-show-image").attr("src", book.thumbnail);
 }
 
 $(($) => {
-	$('#book-add-isbn').enterKey(ev => {
-		searchIsbn(ev.target.value);
-	});
+    $('#book-add-isbn').enterKey(ev => {
+        searchIsbn(ev.target.value);
+    });
 
-	$('#book-add-isbn-search').click(() => {
-		searchIsbn($('#book-add-isbn').val());
-	});
+    $('#book-add-isbn-search').click(() => {
+        searchIsbn($('#book-add-isbn').val());
+    });
 
-	$('#book-add-image').change(event => {
-		$("#book-add-show-image").attr("src", event.target.value);
-	});
+    $('#book-add-image').change(event => {
+        $("#book-add-show-image").attr("src", event.target.value);
+    });
 
-	$('#type-add').change(event => {
-		const type = event.target.value;
+    $('#type-add').change(event => {
+        const type = event.target.value;
 
-		$(`.form-add`).css('display', 'none');
+        $(`.form-add`).css('display', 'none');
 
-		if (!type) {
-			return;
-		}
+        if (!type) {
+            return;
+        }
 
-		$(`#${type}-add`).css('display', 'block');
-	})
+        $(`#${type}-add`).css('display', 'block');
+    })
 });
