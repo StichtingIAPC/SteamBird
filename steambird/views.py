ca2c665f956d49cb8ae08dedcb1865d110427d8e
Luuk Lenders <luuklsll@gmail.com>
2019-09-02 00:09:12 +0200

    Added the errors directory name
    Also small gitignore update to ignore possible files collected after mange.py collectstatic

--- steambird/views.py
+++ steambird/views.py
@@ -2,25 +2,25 @@ from django.shortcuts import render
 from django.views import View
 
 
-def handler404(request, exception=None, template_name="404.html"):
+def handler404(request, exception=None, template_name="errors/404.html"):
     return render(request, template_name=template_name, status=404, context={
         'exception': exception,
     })
 
 
-def handler500(request, exception=None, template_name="500.html"):
+def handler500(request, exception=None, template_name="errors/500.html"):
     return render(request, template_name=template_name, status=500, context={
         'exception': exception,
     })
 
 
-def permission_denied(request, exception=None, template_name="403.html"):
+def permission_denied(request, exception=None, template_name="errors/403.html"):
     return render(request, template_name=template_name, status=403, context={
         'exception': exception,
     })
 
 
-def bad_request(request, exception=None, template_name="400.html"):
+def bad_request(request, exception=None, template_name="errors/400.html"):
     return render(request, template_name=template_name, status=400, context={
         'exception': exception,
     })
